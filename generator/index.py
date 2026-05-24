49d88476cad71e1e82894a3bd0a4f343c05a1790
victors3136 <victor.selegean@gmail.com>
2026-05-24 23:35:57 +0300

    Lower speed lower bound for generator

--- generator/index.py
+++ generator/index.py
@@ -17,7 +17,7 @@ def handler(event, context):
         "timestamp": int(time.time() * 1000),
         "latitude": round(cluj_napoca_position["latitude"] + (random.random() - 0.5) * 0.1, 4),
         "longitude": round(cluj_napoca_position["longitude"] + (random.random() - 0.5) * 0.1, 4),
-        "speed_kmh": random.randint(15, 70)  # kmph
+        "speed_kmh": random.randint(1, 70)  # kmph
     }
 
     try:
