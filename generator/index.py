25042d70ed235c308a0f010360dcf3e897e65a0c
victors3136 <victor.selegean@gmail.com>
2026-05-25 07:25:28 +0300

    Move back from batch event generation

--- generator/index.py
+++ generator/index.py
@@ -12,28 +12,23 @@ cluj_napoca_position = {
 
 
 def handler(event, context):
-    error = False
-    for _ in range(6):
-        generated_event = {
-            "stream_id": f"car_{random.randint(0, 1_000_000)}",
-            "timestamp": int(time.time() * 1000),
-            "latitude": round(cluj_napoca_position["latitude"] + (random.random() - 0.5) * 0.1, 4),
-            "longitude": round(cluj_napoca_position["longitude"] + (random.random() - 0.5) * 0.1, 4),
-            "speed_kmh": round(random.triangular(1, 70, 25))  # kmph
-        }
-
-        try:
-            simple_queue_service.send_message(
-                QueueUrl=os.environ['QUEUE_URL'],
-                MessageBody=json.dumps(generated_event)
-            )
-        except Exception as internal_server_error:
-            error = True
-            print(f"Error logging event to SQS: {str(internal_server_error)}")
-            break
-
-        time.sleep(10)
-    return {
-        "body": json.dumps("Batch of radar events successfully queued.") \
-            if error else json.dumps("Error when trying to dump an event batch")
+    generated_event = {
+        "stream_id": f"car_{random.randint(0, 1_000_000)}",
+        "timestamp": int(time.time() * 1000),
+        "latitude": round(cluj_napoca_position["latitude"] + (random.random() - 0.5) * 0.1, 4),
+        "longitude": round(cluj_napoca_position["longitude"] + (random.random() - 0.5) * 0.1, 4),
+        "speed_kmh": round(random.triangular(1, 70, 25))  # kmph
     }
+
+    try:
+        simple_queue_service.send_message(
+            QueueUrl=os.environ['QUEUE_URL'],
+            MessageBody=json.dumps(generated_event)
+        )
+        return {
+            "statusCode": 200,
+            "body": json.dumps("Radar event successfully queued.")
+        }
+    except Exception as internal_server_error:
+        print(f"Error logging event to SQS: {str(internal_server_error)}")
+        return {"statusCode": 500, "body": str(internal_server_error)}
