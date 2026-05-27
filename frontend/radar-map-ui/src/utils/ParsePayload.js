666555486731ab521313c23f4f0612f8d0dd64fa
victors3136 <victor.selegean@gmail.com>
2026-05-27 16:49:34 +0300

    Remove references to mock data

--- frontend/radar-map-ui/src/utils/ParsePayload.js
+++ frontend/radar-map-ui/src/utils/ParsePayload.js
@@ -1,13 +1,12 @@
 export const parsePayload = (rawData) => rawData.map(item => {
-    const rawStreamId = item.stream_id?.Value || item.stream_id || 'unknown';
+    const id = item.stream_id?.Value || item.stream_id || 'unknown';
     const rawLat = item.latitude?.Value || item.x || 0;
     const rawLng = item.longitude?.Value || item.y || 0;
     const rawSpeed = item.speed?.Value || item.speed || 0;
     const rawTime = item.timestamp?.Value || item.timestamp || Date.now();
-    const cleanId = rawStreamId.includes("mock") ? rawStreamId : `${rawStreamId} (Live)`;
 
     return {
-        id: cleanId,
+        id: id,
         timestamp: new Date(Number(rawTime)),
         lat: Number(rawLat),
         lng: Number(rawLng),
